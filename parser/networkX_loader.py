6819f1de2f7d952444ae5e1a091d82db280139b9
jaxels20 <xfm82gtn@gmail.com>
2023-02-17 11:23:25 +0100

    working on it

--- parser/networkX_loader.py
+++ parser/networkX_loader.py
@@ -18,20 +18,27 @@ def bfs(graph, start, depth=1):
                 queue.extend(set(graph[vertex]) - visited)
             depth -= 1
     return visited
-    
-""" 
-visited_nodes = bfs(G, "Cellular or Molecular Interactions [MoA]", depth=1)
 
-subgraph = G.subgraph(visited_nodes)
 
-pos = nx.circular_layout(subgraph)
-nx.draw_networkx_nodes(subgraph, pos, nodelist=subgraph, node_color='r', node_size=500, alpha=0.8)
-nx.draw_networkx_edges(subgraph, pos, width=1.0, alpha=0.5)
-nx.draw_networkx_labels(subgraph, pos, font_size=10, font_family='sans-serif')
-nx.draw_networkx_edge_labels(subgraph, pos, edge_labels=nx.get_edge_attributes(subgraph, "association_type"), font_size=8, font_family='sans-serif')
- """
+def set_edge_attrib(edge):
+    edge_attributes = {}
+    # create dictionary of attributes
+    edge_attributes["association_type"] = edge.find("name").text
+    edge_attributes["namespace"] = edge.find("namespace").text
+    edge_attributes["from_namespace"] = edge.find("from_namespace").text
+    edge_attributes["to_namespace"] = edge.find("to_namespace").text
+    edge_attributes["from_name"] = edge.find("from_name").text
+    edge_attributes["to_name"] = edge.find("to_name").text
+    edge_attributes["from_code"] = edge.find("from_code").text
+    edge_attributes["to_code"] = edge.find("to_code").text
 
-def get_networkx_graph(remove_self_loops=False, remove_isolated_nodes=False):
+    return edge_attributes
+
+def conditionally_add_node(G, node, node_attributes):
+    if G.has_node(node) == False:
+        G.add_node(node, **node_attributes)
+
+def get_networkx_graph(remove_self_loops=False, remove_isolated_nodes=False, bipartite=False):
 
     # get the path of the xml file
     path = Path(__file__).resolve().parents[1] / "med-rt" / "Core_MEDRT_2023.02.06_XML.xml"
@@ -43,55 +50,90 @@ def get_networkx_graph(remove_self_loops=False, remove_isolated_nodes=False):
     # create a directed graph
     G = nx.DiGraph()
 
-    # add nodes to the graph
-    for elem in root.findall('term'):
-        node_name = elem.find('name').text
+    if not bipartite:
+        # add concepts as nodes to the graph from the xml file
+        for element in root.findall('concept'):
+            attributes = {}
+            node_name = element.find("name").text
+            
+            for prop in element.findall('property'):
+                if prop.find('name').text == "CTY":
+                    attributes["type"] = prop.find('value').text
 
-        # create dictionary of attributes
-        attributes = dict(zip([subelem.tag for subelem in elem.iter()], [elem.text for elem in elem.iter()]))
-        
-        # remove the name attribute and the term tag
-
-        attributes.pop('name')
-        attributes.pop('term')
-
-        attributes['type'] = "drug"
-        
-        G.add_node(node_name, **attributes)
-
-    for elem in root.findall('concept'):
-        node_name = elem.find('name').text
-
-        attributes = dict(zip([subelem.tag for subelem in elem.iter()], [elem.text for elem in elem.iter()]))
-
-        attributes.pop('name')
-        attributes.pop('concept')
-
-        attributes['type'] = "concept"
-
-        G.add_node(node_name, **attributes)
+            # add the nodes with their attributes
+            G.add_node(node_name, **attributes)
 
 
-    # add edges to the graph
+    # add edges to the graph from the xml file this also adds the nodes
     for edge in root.findall('association'):
         try: 
-            attributes = {}
-            node1 = edge.find("from_name").text
-            node2 = edge.find("to_name").text
+            from_node_attributes = {}
+            to_node_attributes = {}
 
-            # create dictionary of attributes
-            attributes["association_type"] = edge.find("name").text
-            attributes["namespace"] = edge.find("namespace").text
-            attributes["from_namespace"] = edge.find("from_namespace").text
-            attributes["to_namespace"] = edge.find("to_namespace").text
-            attributes["from_name"] = edge.find("from_name").text
-            attributes["to_name"] = edge.find("to_name").text
-            attributes["from_code"] = edge.find("from_code").text
-            attributes["to_code"] = edge.find("to_code").text
+            from_node = edge.find("from_name").text
+            to_node = edge.find("to_name").text
+
+            edge_attributes = set_edge_attrib(edge)
+
+            if edge_attributes["association_type"] == "may_treat" or edge_attributes["association_type"] == "may_prevent":
+                from_node_attributes["type"] = "drug"
+                to_node_attributes["type"] = "disease"
             
-            G.add_edge(node1, node2, **attributes)
+
+                # add the nodes with their attribute
+                
+                conditionally_add_node(G, from_node, from_node_attributes)
+                conditionally_add_node(G, to_node, to_node_attributes)
+
+                G.add_edge(from_node, to_node, **edge_attributes)
+            
+            if not bipartite:
+
+                if edge_attributes["association_type"] == "has_SC":
+                    from_node_attributes["type"] = "drug"
+                    to_node_attributes["type"] = "SC"
+
+                    # add the nodes with their attributes
+                    conditionally_add_node(G, from_node, from_node_attributes)
+                    conditionally_add_node(G, to_node, to_node_attributes)
+
+                    G.add_edge(from_node, to_node, **edge_attributes)
+
+                elif edge_attributes["association_type"] == "has_MoA":
+                    from_node_attributes["type"] = "drug"
+                    to_node_attributes["type"] = "MoA"
+
+                    # add the nodes with their attributes
+                    conditionally_add_node(G, from_node, from_node_attributes)
+                    conditionally_add_node(G, to_node, to_node_attributes)
+
+                    G.add_edge(from_node, to_node, **edge_attributes)
+
+                elif edge_attributes["association_type"] == "has_PE":
+                    from_node_attributes["type"] = "drug"
+                    to_node_attributes["type"] = "PE"
+
+                    # add the nodes with their attributes
+                    conditionally_add_node(G, from_node, from_node_attributes)
+                    conditionally_add_node(G, to_node, to_node_attributes)
+
+                    G.add_edge(from_node, to_node, **edge_attributes)
+
+                elif edge_attributes["association_type"] == "has_TC":
+                    from_node_attributes["type"] = "drug"
+                    to_node_attributes["type"] = "TC"
+
+                    # add the nodes with their attributes
+                    conditionally_add_node(G, from_node, from_node_attributes)
+                    conditionally_add_node(G, to_node, to_node_attributes)
+
+                    G.add_edge(from_node, to_node, **edge_attributes)
+
+                elif edge_attributes["association_type"] == "Parent Of":
+                    G.add_edge(from_node, to_node, **edge_attributes)
+                    
+
         except:
-            pass
             print("Error: ", edge.find("from_name").text, edge.find("to_name").text)
 
 
@@ -105,7 +147,18 @@ def get_networkx_graph(remove_self_loops=False, remove_isolated_nodes=False):
     return G
 
 if __name__ == "__main__":
-    G = get_networkx_graph(remove_self_loops=True, remove_isolated_nodes=True)
+    G = get_networkx_graph(remove_self_loops=True, remove_isolated_nodes=True, bipartite=False)
+
+    # get the subgraph with a depth of 2
+    subgraph = bfs(G, "Antirheumatic Agent [EPC]", depth=1)
+    pos = nx.spring_layout(G.subgraph(subgraph))
+
+    print(G.nodes["Antirheumatic Agent [EPC]"]["type"])
+    # draw the subgraph with edge labels and node attributes
+    nx.draw(G.subgraph(subgraph), with_labels=True)
+    nx.draw_networkx_edge_labels(G.subgraph(subgraph), pos=pos, edge_labels=nx.get_edge_attributes(G.subgraph(subgraph), 'association_type'))
+    nx.draw_networkx_labels(G.subgraph(subgraph), pos=pos, labels=nx.get_node_attributes(G.subgraph(subgraph), 'type'))
+    plt.show()
 
 
 
